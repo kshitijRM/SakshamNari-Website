3351d49797dc8017b8ae6d887b6877dce17a2115
Kshitij <kshitijdinni6605@gmail.com>
2026-03-15 15:18:45 +0530

    database setup

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 /* ---------------- Middleware ---------------- */
 app.use(cors({
@@ -13,7 +22,7 @@ app.use(cors({
 app.use(express.json());
 
 /* ---------------- Port ---------------- */
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 /* ---------------- Database Connection ---------------- */
 const db = mysql.createConnection({
@@ -75,5 +84,5 @@ app.post("/login", (req, res) => {
 
 /* ---------------- Start Server ---------------- */
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
